78976200757adf62a4338a9cdb98eb3e77e8081a
Neeraj <59769298+Developer-Neeraj@users.noreply.github.com>
2024-11-27 00:29:28 +0530

    Update secret.jsx

--- client/secret.jsx
+++ client/secret.jsx
@@ -5,7 +5,7 @@ let apiHostName;
 if (window.location.hostname === "localhost") {
   apiHostName = "http://localhost:5100/api"; // Set your local API URL here
 } else {
-  apiHostName = "https://task-management-46e6.vercel.app/"; // Set your Vercel API URL here
+  apiHostName = "https://task-management-46e6.vercel.app/api"; // Set your Vercel API URL here
 }
 
 export default apiHostName;
